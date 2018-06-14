999b52e2759380426aae284b52d43ea6fe67cc8a
Elang Surya <elangsurya19@gmail.com>
2018-06-14 21:20:22 +0700

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CodeIgnter Tutorial</title>
+</head>
+<body>
+
+    <h1><?php echo $title; ?></h1>
+    
---
+++ application/views/v_login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Login Dengan CodeIgniter</title>
+</head>
+<body>
+    <h1>Login Dengan CodeIgniter</h1>
+    <form action="<?php echo base_url('login/aksi_login'); ?>" method="post">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Login"></td>
+            </tr>
+        </table>
+    </form>
+
+</body>
+</html>
